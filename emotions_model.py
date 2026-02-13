28762229fc24617fd8622a9d5fd40f5fb81322f4
NewArielstern <arielstern.ayridigital@gmail.com>
2026-02-13 12:39:23 +0200

    Update emotions_model.py

--- emotions_model.py
+++ emotions_model.py
@@ -63,10 +63,10 @@ model = Sequential([
 
     Conv2D(32, 3, activation='relu', padding='same'),
     Conv2D(64, 3, activation='relu', padding='same'),
-    MaxPooling2D(2),
+    MaxPooling2D(pool_size=2,strides = 2),
 
     Conv2D(128, 3, activation='relu', padding='same'),
-    MaxPooling2D(2),
+    MaxPooling2D(pool_size=2,strides = 2),
 
     Conv2D(128, 3, activation='relu', padding='same'),
 
@@ -102,3 +102,4 @@ model.save(model_path)
 print(f'Model {model_path} Saved Successfully!')
 
 # 378/378 ━━━━━━━━━━━━━━━━━━━━ 144s 382ms/step - accuracy: 0.6610 - loss: 0.8680 - val_accuracy: 0.6591 - val_loss: 0.8602
+
